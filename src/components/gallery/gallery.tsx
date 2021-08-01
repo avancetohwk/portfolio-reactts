2f54211b4569d3a94c850673646a4a0e5259462b
avancetohwk <73689768+avancetohwk@users.noreply.github.com>
2021-08-01 17:59:47 +0800

    gallery

---
+++ src/components/gallery/gallery.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import Masonry from 'react-masonry-component';
+import './gallery.scss'
+
+
+const imagesLoadedOptions = { background: '.my-bg-image-el' }
+
+const PHOTOS = [
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/14/03/09/women-5296386_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/14/11/23/wanderer-5297457_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/18/18/53/the-cliffs-5314651_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/19/09/16/fantasy-5316369_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/18/09/42/flowers-5312741_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/06/20/10/26/plant-5320443_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/02/25/19/16/stawberry-4879794_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/12/11/15/12/dogs-4688586_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2018/05/26/10/54/strawberries-3431122_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2017/10/18/16/08/wolf-2864647_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2020/02/25/19/16/stawberry-4879794_960_720.jpg",
+    },
+    {
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/12/11/15/12/dogs-4688586_960_720.jpg",
+    },
+  ];
+
+  const masonryOptions = {
+    fitWidth: true,
+    // columnWidth: 300,
+    // gutter: 30,
+    itemSelector: ".photo-item",
+  };
+
+class Gallery extends React.Component {
+    render() {
+        const childElements = PHOTOS.map(function(element){
+           return (
+                <li className="photo-item">
+                    <img src={element.imageUrl} />
+                </li>
+            );
+        });
+    
+        return (
+            <Masonry
+                className={'photo-list'} // default ''
+                elementType={'div'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                //imagesLoadedOptions={imagesLoadedOptions} // default {}
+            >
+                {childElements}
+            </Masonry>
+        );
+    }
+}
+
+export default Gallery;
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import Gallery from "../components/gallery/gallery";
 
 class Work extends Component {
 
     render(){
         return (
-            <div>
-                Work
+            <div className="page" id="work-page">
+                <Gallery></Gallery>
             </div>
         )
     }
