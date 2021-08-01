052bf5701def9c5350a7ce0ebb0a5cdd154e3941
avancetohwk <73689768+avancetohwk@users.noreply.github.com>
2021-08-01 16:22:14 +0800

    responsive about

--- src/components/skillschart/skillschart.tsx
+++ src/components/skillschart/skillschart.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { isPropertySignature, skipPartiallyEmittedExpressions } from 'typescript';
 import Logo from '../assets/logo-edited.png';
 import './skillschart.scss'
 
@@ -6,36 +7,76 @@ type Props = {
   children?: any;
 }
 
-export default function SkillsChart(props: Props) {
-  return (
-    <>
-        <div className="skills ">
-            <div className="charts">
-                <div className="chart chart--dev">
-                <span className="chart__title">High Risk Indicators				
-            </span>
-                <ul className="chart--horiz">
-                    <li className="chart__bar" style={{width: '98%'}}>
-                    <span className="chart__label">
-                    History of family violence
-                    </span>
-                    </li>
-                    <li className="chart__bar" style={{width: '58%'}}>
-                    <span className="chart__label">
-                    Imminence
-                    </span>
-                    </li>
-                    
-                </ul>
-                </div>
-                
+type State = {
+  skills: any[]
+}
 
-            </div>
-        </div>
-        {props.children}
+export default class SkillsChart extends Component<Props,State> {
+  
+  constructor(props:Props){
+    super(props);
+    this.state = {
+      skills: []
+    }
+  }
+
+  componentDidMount(){
+    let skillsData = [
+      {name: "Adobe Skills", percentage: "75%"},
+      {name: "Animation", percentage: "65%"},
+      {name: "Sketching", percentage: "98%"},
+      {name: "Character Drawing", percentage: "87%"},
+      {name: "Composition", percentage: "92%"},
+    ]
+
+    this.setState({skills: skillsData});
+  }
+
+
+  render(){
+    const {skills} = this.state;
+
+    return (
+      <>
+          <div className="skills ">
+              <div className="charts">
+                  <div className="chart chart--dev">
+                  {/* <span className="chart__title">High Risk Indicators</span> */}
+              <ul className="chart--horiz">
+                {
+                  skills.map(s=>{
+                    return (
+                        <li className="chart__bar" style={{width: s.percentage}}>
+                          <span className="chart__label">
+                            {s.name}
+                          </span>
+                        </li>
+                    )
+                  })
+                }
+                  {/* <ul className="chart--horiz">
+                      <li className="chart__bar" style={{width: '98%'}}>
+                      <span className="chart__label">
+                      History of family violence
+                      </span>
+                      </li>
+                      <li className="chart__bar" style={{width: '58%'}}>
+                      <span className="chart__label">
+                      Imminence
+                      </span>
+                      </li> */}
+                      
+                  </ul>
+                  </div>
+                  
+  
+              </div>
+          </div>
+          
+          
         
-        
-      
-    </>
-  )
+      </>
+    )
+  }
+  
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,12 +30,13 @@ class About extends Component<RouteComponentProps> {
                     <div className="inner-card">
                         <div className="inner-card-content">
                             <div id="text-container">
+                                <h1>About Me</h1>
                                 <p>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ligula magna. Nullam in faucibus leo. Cras ac fermentum felis. Donec ornare sodales leo ut consequat. Proin nec convallis leo. In mi nisl, placerat eu tempus sit amet, pulvinar at sapien. Donec at enim eu nisl fermentum congue. Vestibulum bibendum maximus volutpat. Suspendisse potenti. Duis semper dui ac commodo vehicula. Pellentesque dui diam, ornare ac tortor et, accumsan dictum neque. Quisque gravida sem sit amet vehicula maximus. Fusce non ante eu sapien viverra rutrum a ac nisl. Aliquam aliquam turpis at ultricies bibendum. Sed eleifend libero eu congue euismod. Aenean eget leo fermentum, feugiat ex at, imperdiet felis.
                                 </p>
                             </div>
-                            <div id="skills-container" className="skills">
-                                
+                            <div id="skills-container" >
+                                <h1>Skills</h1>
                                 <SkillsChart></SkillsChart>
                                 
 
