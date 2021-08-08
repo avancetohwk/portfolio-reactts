6d13b9c98c0efe97fc181787da9331874e123829
avancetohwk <73689768+avancetohwk@users.noreply.github.com>
2021-08-08 22:16:43 +0800

    form

--- src/hooks/useStorage.tsx
+++ src/hooks/useStorage.tsx
@@ -33,35 +33,38 @@ export const UploadFiles = (files:any[])=>{
     
     const collectionRef = projectFirestore.collection('images');
     useEffect(() => {
-        Promise.all(
-            // Array of "Promises"
-            [...files].map(file => {
-                const storageRef = projectStorage.ref(file.name);
-                storageRef.put(file).on('state_changed', (snap)=>{
-                    let percentage = (snap.bytesTransferred/ snap.totalBytes) * (100/files.length);
-                    setProgress(progress +percentage);
-                    return percentage;
-                }, (err)=>{
-                    setError(err);
-                }, async()=>{
-                    const url = await storageRef.getDownloadURL();
-                    const createdAt = timestamp();
-                    collectionRef.add({url, createdAt})
-                    setUrl(url);
-                    return progress;
+        if(files){
+            Promise.all(
+                // Array of "Promises"
+                files.map(file => {
+                    const storageRef = projectStorage.ref(file.name);
+                    storageRef.put(file).on('state_changed', (snap)=>{
+                        let percentage = (snap.bytesTransferred/ snap.totalBytes) * (100/files.length);
+                        setProgress(progress +percentage);
+                        return percentage;
+                    }, (err)=>{
+                        setError(err);
+                    }, async()=>{
+                        const url = await storageRef.getDownloadURL();
+                        const createdAt = timestamp();
+                        collectionRef.add({url, createdAt})
+                        setUrl(url);
+                        return progress;
+                    })
                 })
-            })
-          )
-          .then((res) => {
-            console.log(res)
-            console.log(`All success`)
-            // const {progress, url} = res;
-          })
-          .catch((error) => {
-            console.log(`Some failed: `, error.message)
-          }).finally(()=>{
-              //return {progress, url};
-          })
+              )
+              .then((res) => {
+                console.log(res)
+                console.log(`All success`)
+                // const {progress, url} = res;
+              })
+              .catch((error) => {
+                console.log(`Some failed: `, error.message)
+              }).finally(()=>{
+                  //return {progress, url};
+              })
+        }
+        
     }, [files])
     
 }
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,76 +1,116 @@
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
 import ProgressBar from "../components/progressbar/progressbar";
 import { useForm, SubmitHandler } from "react-hook-form";
 import './admin.scss'
+import UploadFiles from "../hooks/useStorage";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 type Inputs = {
     example: string,
     exampleRequired: string,
-    file:FileList
+    file:File[]
 };
 
-const Admin = () => {
+const Admin: React.FC = () => {
     const [files, setFile] = useState<any | null>(null);
     const [error, setError] = useState<any | null>(null);
     const { register, setValue, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const [selectedFiles, setSelectedFiles] = useState<any|null>();
+    const [thumbnails, setThumbnails] = useState<any|null>();
+    const [uploadData, setUploadData] = useState<any|null>();
+    const [toggleChecked, setToggleChecked] = useState<boolean>(false);
 
     const onSubmit: SubmitHandler<Inputs> = data => {
         console.log(data)
+        setUploadData(data.file);
     };
 
+    //runs everytime selectedFile is updated
+    useEffect(() => {
+        if (!selectedFiles) {
+            return
+        }
+
+        const thumbnails = selectedFiles && selectedFiles.map(function(element:File){
+            return (
+                <div className="thumbnail" >
+                    <img className="thumbnail-img" data-img-name={element.name}  src={URL.createObjectURL(element)} />
+                    <div className="thumbnail-overlay" onClick={thumbnailDeleteHandler} data-img-name={element.name}>
+                    <FontAwesomeIcon className="thumbnail-icon"  icon={faTimesCircle} />
+                        
+                    </div>
+                    
+                    {/* <button onClick={thumbnailDeleteHandler}>x</button> */}
+                </div>
+                
+               
+            );
+        });
+        setThumbnails(thumbnails);
+    }, [selectedFiles])
+
+    const thumbnailDeleteHandler = (e:any)=>{
+        console.log(selectedFiles)
+        let fileName = e.currentTarget.getAttribute("data-img-name");
+        if(fileName){
+            setSelectedFiles(selectedFiles.filter((f:File) => f.name !== fileName));
+        }
+        console.log(e.currentTarget.getAttribute("data-img-name"))
+        console.log(selectedFiles)
+    }
+   
 
     const changeHandler = (e: any) =>{
-        let selected = e.target.files;
-        setValue("file", selected)
-        // if(selected){
-        //     setFile(selected);
-        //     setError("");
-        // }else{
-        //     setFile(null);
-        //     setError("Please select an image file (png or jpeg)");
-        // }
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFiles(undefined)
+            setFile(null);
+            setError("Please select an image file (png or jpeg)");
+            return
+        }
 
+        let selectedFilesArray = [...e.target.files];
+        setSelectedFiles(selectedFilesArray); //to create thumbnails
+        //setValue("file", selectedFilesArray); //to add to react-hook-form submit data
     }
 
+    UploadFiles(uploadData);
+    
     return (
-        // <div>
-        //     <input
-        //     type='file'
-        //     id='photo'
-        //     name='photo'
-        //     multiple={true}
-        //     accept='image/png, image/jpeg'
-        //     onChange={changeHandler}
-        //     ></input>
         //     <div className="output">
         //         {error && <div className="error">{error}</div>}
         //         {/* { files && <div>{file.name}</div>} */}
         //         { files && <ProgressBar files={files} setFile={setFile}></ProgressBar>}
         //     </div>
-        // </div>
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        
+
         <div className="page" id="admin-page">
             <div className="card">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="input">
-                        <input type="text" className="input-field" value="Alexander Parkinson" required/>
-                        <label className="input-label">Full name</label>
+                    
+
+                    <div className="input toggle-container">
+                        <input className="toggle" type="checkbox" name="switch" id="switch" defaultChecked={toggleChecked}
+        onChange={() => setToggleChecked(!toggleChecked)}></input>
+                        <label className="toggle-label" htmlFor="switch"></label>
                     </div>
-
-                    <input defaultValue="test" {...register("example")} />
-                    <input 
-                        required={true}
-                        type="file"
-                        {...register("file")}
-                        id='photo'
-                        name='photo'
-                        multiple={true}
-                        accept='image/png, image/jpeg'
-                        onChange={changeHandler}
-                    />
-
-          
-                    <div className="form-group file-area">
-                            <label >Images <span>Your images should be at least 400x300 wide</span></label>
+                    <div>
+                        {toggleChecked && 
+                            <div className="input">
+                                <select className="input-field" id="category-select" name="category">
+                                    <option value="volvo">Volvo</option>
+                                    <option value="saab">Saab</option>
+                                    <option value="fiat">Fiat</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                            </div>
+                        }
+                        {!toggleChecked && 
+                            <div className="input">
+                                <input type="text" className="input-field" value="Alexander Parkinson" required/>
+                            </div>
+                        }
+                    </div>
+                    <div className="input file-area">
                         <input type="file"  id="images" required={true} multiple={true} {...register("file")}
              
                         accept='image/png, image/jpeg'
@@ -84,6 +124,10 @@ const Admin = () => {
                     
                     <input type="submit" />
                 </form>
+                <div className="thumbnails-container">
+                    {thumbnails}
+                </div>
+                
             </div>
             
         </div>
